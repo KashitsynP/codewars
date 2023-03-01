4c57f75a60e1eaa7f2d06ce4a1fce4fd835a5b5c
Pavel Kashitsyn <kashitsyn@gmail.com>
2023-03-01 05:34:27 +0400

    Indexed capitalization

---
+++ 7_kyu/Indexed capitalization.py
@@ -0,0 +1,17 @@
+# Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+# For example:
+
+# capitalize("abcdef",[1,2,5]) = "aBCdeF"
+# capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+# The input will be a lowercase string with no spaces and an array of digits.
+
+####################################################################################################################
+
+# My solution:
+
+def capitalize(s, ind):
+    s1 = ''
+    for i in range(len(s)):
+        s1 += s[i].upper() if i in ind else s[i]
+    return s1
